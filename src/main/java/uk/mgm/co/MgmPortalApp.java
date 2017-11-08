1a6e5c1c9872249287f0376a37b2301dd6cb1ff5
Abid Ur-Rehman <abid.ur-rehman1@dwp.gsi.gov.uk>
2017-11-08 15:29:09 +0000

    Changed app name

--- src/main/java/uk/mgm/co/MgmPortalApp.java
+++ src/main/java/uk/mgm/co/MgmPortalApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MgmApplication {
+public class MgmPortalApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MgmApplication.class, args);
+		SpringApplication.run(MgmPortalApp.class, args);
 	}
 }
